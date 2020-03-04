8ce6dd668e263004bbdcd718ff3f149fd5d431d3
John Barhorst <crashdsinc@comcast.net>
2020-03-03 22:57:13 -0600

    basic route scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navigation from './Components/Navigation';
+import Main from './Components/Main';
+import About from './Components/About';
+import Portfolio from './Components/Portfolio';
 import './App.css';
 
 function App() {
@@ -8,6 +11,17 @@ function App() {
     <Router>
       <div className="App">
         <Navigation />
+        <Switch>
+          <Route path='/portfolio'>
+            <Portfolio />
+          </Route>
+          <Route path='/about' >
+            <About />
+          </Route>
+          <Route exact path='/' >
+            <Main />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
