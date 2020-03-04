1f0a172bdc9d17f733cd2c0e7a881f5f4743706a
John Barhorst <crashdsinc@comcast.net>
2020-03-03 23:08:00 -0600

    small animation playing

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Portfolio = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <Section>
+      <motion.h1
+        initial={{
+          y: -25,
+          opacity: 0
+        }}
+        animate={{
+          y: 0,
+          opacity: 1
+        }}
+        transition={{
+          duration: 1
+        }}
+      >Portfolio</motion.h1>
+    </Section>
   )
 }
 
 export default Portfolio;
+
+const Section = styled(motion.section)`
+  text-align: center;
+`;
+
