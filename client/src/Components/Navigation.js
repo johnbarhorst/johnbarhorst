58f75be6474e50c175639b68dd7b4a9fd7c5c04e
John Barhorst <crashdsinc@comcast.net>
2020-03-03 22:51:04 -0600

    github logos, nav playing

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -8,8 +8,8 @@ const Navigation = () => {
   return (
     <nav>
       <Ul>
-        <li><Link to="/">John Barhorst</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/">J B</Link></li>
+        <li><Link to="/about">About Me</Link></li>
         <li><Link to="/portfolio">Portfolio</Link></li>
       </Ul>
     </nav>
@@ -21,15 +21,15 @@ export default Navigation;
 
 const Ul = styled.ul`
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   align-content: center;
   list-style: none;
   padding: 0 10px;
   li {
     margin: 0 5px;
-    &:first-child {
+    /* &:first-child {
     margin-right: auto;
-      }
+      } */
   a {
     text-decoration: none;
     color: #333;
