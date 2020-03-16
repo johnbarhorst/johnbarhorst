ee2989154ac011e5878e3c443e80c645fb581d68
John Barhorst <crashdsinc@comcast.net>
2020-03-16 18:13:13 -0500

    formatted time is important!

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express();
 const path = require('path');
 const port = 3001;
 
+const serverListenTime = function () {
+  const now = new Date();
+  const addZero = i => {
+    return i < 10 ? '0' + i : i;
+  }
+  const time = `${addZero(now.getHours())}:${addZero(now.getMinutes())}:${addZero(now.getSeconds())}`;
+  return time;
+}
+
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-app.listen(port, console.log('Listening on port: ' + port))
+app.listen(port, console.log(`Listening on port: ${port} at ${serverListenTime()}`));
