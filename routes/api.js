0591ef34e5550c9cc481a2847a064d8880b4202b
John Barhorst <crashdsinc@comcast.net>
2020-03-16 20:46:01 -0500

    switched to node-fetch request depr

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,54 @@
 const router = require('express').Router();
+const fetch = require('node-fetch');
+require('dotenv').config();
+const APIKEY = process.env.APIKEY;
 
-router.use('/search', (req, res, next) => {
-  const json = JSON.stringify({ result: 'Test' });
-  res.send(json);
+// Hash converter for sqlite look ups
+const convertHash = hash => {
+  let x = parseInt(hash);
+  if (x > 0xFFFFFFFF) {
+    console.error('Too big, must have a wrong number');
+
+  }
+  if (x > 0x7FFFFFFF) {
+    x = 0x100000000 - x;
+    if (x < 2147483648) {
+      return -x
+    }
+    else {
+      return -2147483648
+    }
+  }
+  return x;
+}
+
+const headers = {
+  "X-API-KEY": APIKEY,
+}
+
+function checkStatus(res) {
+  // Bungie has a ton of ErrorCode possibilities, 1 = success!
+  if (res.ErrorCode === 1) {
+    return true
+  } else {
+    return false;
+  }
+}
+
+
+router.use('/search/:displayName', async (req, res, next) => {
+  const searchQuery = await fetch(
+    `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/All/${req.params.displayName}/`,
+    { headers }).then(res => res.json());
+  const responseStatus = checkStatus(searchQuery);
+  if (responseStatus) {
+    const accountList = {
+      accounts: searchQuery.Response
+    };
+    res.send(JSON.stringify(accountList));
+  } else {
+    res.send(JSON.stringify(searchQuery.Message));
+  }
 });
 
 module.exports = router;
