4bd7ee4c199a5f05ce9e5f37159d4634f19ef2d5
John Barhorst <crashdsinc@comcast.net>
2020-03-17 19:33:53 -0500

    logos and background logic

--- client/src/Components/DestinyAccountCard.js
+++ client/src/Components/DestinyAccountCard.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { Card } from '../Elements';
 
+const membershipTypeEnum = ['None', 'Xbox', 'PSN', 'Steam', 'Blizzard', 'Stadia'];
+
 const DestinyAccountCard = ({ account }) => {
   const { displayName, iconPath, membershipType, membershipId } = account;
   return (
-    <Container
+    <Card
       initial={{
         opacity: 0,
         x: 30
@@ -17,15 +19,28 @@ const DestinyAccountCard = ({ account }) => {
       }}
       exit={{ opacity: 0 }}
     >
-      <Card>
+      <Container>
+        <SystemLogo
+          src={`https://www.bungie.net${iconPath}`}
+          alt={`${membershipTypeEnum[membershipType]} logo`}
+          membershipType={membershipType}
+        />
         <h3>{displayName}</h3>
-      </Card>
-    </Container>
+      </Container>
+    </Card>
   )
 }
 
 export default DestinyAccountCard;
 
 const Container = styled(motion.div)`
+  display: flex;
+  align-items: center;
+`;
 
+const SystemLogo = styled(motion.img)`
+  height: 2em;
+  margin-right: 15px;
+  background-color: ${props => props.membershipType === 3 ? '#333' : null};
+  border-radius: 50%;
 `;
