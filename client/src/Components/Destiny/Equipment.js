4776eb33497b5a8a1d7cea5ef9935fd57c62021c
John Barhorst <crashdsinc@comcast.net>
2020-03-31 22:42:45 -0500

    icons

--- client/src/Components/Destiny/Equipment.js
+++ client/src/Components/Destiny/Equipment.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import Item from './Item';
 
 const Equipment = ({ equipment }) => {
   return (
     <div>
       <h3>Equipment</h3>
-      <div>
+      <Wrapper
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         {equipment.map(item => <Item item={item} key={item.name} />)}
-      </div>
+      </Wrapper>
     </div>
   )
 }
 
 export default Equipment;
+
+const Wrapper = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+`;
--- client/src/Components/Destiny/Item.js
+++ client/src/Components/Destiny/Item.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Item = ({ item }) => {
+  const { name, hasIcon, icon } = item;
   return (
     <div>
-      <h5>{item.name}</h5>
+      <h5>{name}</h5>
+      {hasIcon && <img src={`https://www.bungie.net${icon}`} />}
     </div>
   )
 }
