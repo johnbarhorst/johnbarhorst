dd7282d3efbc7fc2ec8c53046a9dcf4a732286ac
John Barhorst <crashdsinc@comcast.net>
2020-04-07 17:20:26 -0500

    custom fetch hook

---
+++ client/src/Hooks/useFetchData.js
@@ -0,0 +1,56 @@
+import { useState, useEffect, useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH': return {
+      ...state,
+      isLoading: true,
+      isError: false,
+    }
+    case 'SUCCESS': return {
+      ...state,
+      isLoading: false,
+      isError: false,
+      data: action.payload
+    }
+    case 'FAIL': return {
+      ...state,
+      isLoading: false,
+      isError: true
+    }
+
+    default:
+      break;
+  }
+}
+
+export const useFetchData = ({ url, options = {} }, initialData) => {
+  const [fetchSettings, setFetchSettings] = useState({ url, options });
+  const [state, dispatch] = useReducer(reducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+    dispatch({ type: 'FETCH' });
+    try {
+      const getData = async () => {
+        const req = await fetch(fetchSettings);
+        const json = await req.json();
+        if (!didCancel) {
+          dispatch({ type: 'SUCCESS', payload: json });
+        }
+      }
+      getData();
+    } catch (error) {
+      dispatch({ type: 'ERROR' });
+    }
+    return () => {
+      didCancel = true;
+    }
+  }, [fetchSettings]);
+
+  return [state, setFetchSettings];
+}
