eb4959a78c78c754fefc12e73275f65b908fcd94
John Barhorst <crashdsinc@comcast.net>
2020-04-13 17:09:27 -0500

    static key for object persistence

--- client/src/Components/Ranking/PreferenceRanking.js
+++ client/src/Components/Ranking/PreferenceRanking.js
@@ -1,39 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { AnimatedButton, Card } from '../../Elements';
 
 const sampleData = [
-  'item1',
-  'item2',
-  'item3',
-  'item4',
-  'item5',
-  'item6',
-  'item7',
-  'item8',
-  'item9',
-  'item10',
-  'item11',
-  'item12',
-  'item13',
-  'item14',
-  'item15',
-  'item16',
-  'item17',
-  'item18',
-  'item19',
-  'item20',
-  'item21',
-  'item22',
-  'item23',
-  'item24',
-  'item25',
-  'item26',
-  'item27',
-  'item28',
-  'item29',
-  'item30',];
+  { text: 'item1', number: 1 },
+  { text: 'item2', number: 2 },
+  { text: 'item3', number: 3 },
+  { text: 'item4', number: 4 },
+  { text: 'item5', number: 5 },
+  { text: 'item6', number: 6 },
+  { text: 'item7', number: 7 },
+  { text: 'item8', number: 8 },
+  { text: 'item9', number: 9 },
+  { text: 'item10', number: 10 },
+  { text: 'item11', number: 11 },
+  { text: 'item12', number: 12 },
+  { text: 'item13', number: 13 },
+  { text: 'item14', number: 14 },
+  { text: 'item15', number: 15 },
+  { text: 'item16', number: 16 },
+  { text: 'item17', number: 17 },
+  { text: 'item18', number: 18 },
+  { text: 'item19', number: 19 },
+  { text: 'item20', number: 20 },
+  { text: 'item21', number: 21 },
+  { text: 'item22', number: 22 },
+  { text: 'item23', number: 23 },
+  { text: 'item24', number: 24 },
+  { text: 'item25', number: 25 },
+  { text: 'item26', number: 26 },
+  { text: 'item27', number: 27 },
+  { text: 'item28', number: 28 },
+  { text: 'item29', number: 29 },
+  { text: 'item30', number: 30 },
+];
 
 
 const wrapNumber = (min, max, num) => {
@@ -41,9 +42,28 @@ const wrapNumber = (min, max, num) => {
   return ((((num - min) % rangeSize) + rangeSize) % rangeSize) + min;
 }
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SELECT_ITEM':
+      return {
+        ...state,
+        option1: state.option1 + 1,
+        option2: state.option2 + 1,
+        list: [...action.payload],
+      }
+
+    default:
+      break;
+  }
+}
+
 const PreferenceRanking = () => {
-  const [list, setList] = useState(sampleData);
-  const [[option1, option2], setOptions] = useState([0, 1]);
+  const [{ option1, option2, preferred, list }, dispatch] = useReducer(reducer, {
+    option1: 0,
+    option2: 1,
+    preferred: [],
+    list: sampleData
+  })
 
   const getRandomItem = () => {
     Math.floor(Math.random() * list.length);
@@ -62,7 +82,9 @@ const PreferenceRanking = () => {
   }
 
   const handleSelection = (value) => {
-    console.log(value);
+    let tempList = [...list];
+    if (value === option2) { [tempList[option1], tempList[option2]] = [tempList[option2], tempList[option1]]; }
+    dispatch({ type: 'SELECT_ITEM', payload: [...tempList] });
   }
   // Make a list of items to rank
   // Take an array of items
@@ -72,11 +94,11 @@ const PreferenceRanking = () => {
   return (
     <motion.div exit={{ opacity: 0 }}>
       <SelectionDisplay>
-        <AnimatedButton onClick={() => handleSelection(option1)}>{list[option1]}</AnimatedButton>
-        <AnimatedButton onClick={() => handleSelection(option2)}>{list[option2]}</AnimatedButton>
+        <AnimatedButton onClick={() => handleSelection(option1)}>{list[option1].text}</AnimatedButton>
+        <AnimatedButton onClick={() => handleSelection(option2)}>{list[option2].text}</AnimatedButton>
       </SelectionDisplay>
       <Card>
-        {list.map((item, i) => <ListItem key={i} colorIndex={i}><h3>{item}</h3></ListItem>)}
+        {list.map(item => <ListItem key={item.number} colorIndex={item.number}><h3>{item.text}</h3></ListItem>)}
       </Card>
     </motion.div>
   )
