405dc1fe0b0456ddb5c3d7a9b84066fafab3a244
John Barhorst <crashdsinc@comcast.net>
2020-04-14 15:07:22 -0500

    form styles

--- client/src/Elements.js
+++ client/src/Elements.js
@@ -14,3 +14,18 @@ export const AnimatedButton = styled(motion.button)`
   border: 1px solid #999;
   padding: 5px 20px;
 `;
+
+export const Form = styled.form`
+font-size: 30px;
+text-align: center;
+margin-bottom: 30px;
+
+input {
+padding: 5px 10px;
+font-size: 20px;
+border-radius: 5px;
+border: 1px solid #999;
+vertical-align: middle;
+margin: 0 20px;
+}
+`;
