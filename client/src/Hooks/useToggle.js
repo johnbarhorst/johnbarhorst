a6ad8cd97d6897bffe2aa3fba032a0d941e6820f
John Barhorst <crashdsinc@comcast.net>
2020-04-14 18:12:08 -0500

    options!

--- client/src/Hooks/useToggle.js
+++ client/src/Hooks/useToggle.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 
-export const useToggle = initial => {
+// Pass second param true to use bracket notation and rename toggle outputs
+
+export const useToggle = (initial, renameable = false) => {
   const [isToggled, setToggle] = useState(initial);
   const toggle = () => setToggle(prev => !prev);
-  return { isToggled, setToggle, toggle };
+  return renameable ? [isToggled, setToggle, toggle] : { isToggled, setToggle, toggle };
 }
