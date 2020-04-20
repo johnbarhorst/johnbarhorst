0bcb0beba9c895a94ba17debc735691ac30cce9b
John Barhorst <crashdsinc@comcast.net>
2020-04-20 18:10:46 -0500

    extra AnimatePresence stops animating presence

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,75 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Menu from './Menu';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useAppContext } from '../State';
-import { H1, H2, H3 } from '../Elements';
+import { H1, H2, Wrapper } from '../Elements';
 
 const Main = () => {
-  const { isNavOpen } = useAppContext();
   return (
-    <AnimatePresence>
-      <Wrapper
-        initial={{
-          opacity: 0
-        }}
-        animate={{
-          opacity: 1
-        }}
-        exit={{
-          opacity: 0
-        }}
-        transition={{
-          damping: 300
-        }}
-      >
-        <section>
-          <H1>I'm John</H1>
-          <p>I build rad stuff for the web.</p>
-          <ProfilePic src="./img/meandv.jpg" alt="A bearded, bespectacled man with his dog" />
-        </section>
+    <Wrapper
+      initial={{
+        opacity: 0
+      }}
+      animate={{
+        opacity: 1
+      }}
+      exit={{
+        opacity: 0
+      }}
+    >
+      <section>
+        <H1>I'm John</H1>
+        <p>I build rad stuff for the web.</p>
+        <ProfilePic src="./img/meandv.jpg" alt="A bearded, bespectacled man with his dog" />
+      </section>
 
-        <section>
-          <H2>What I Use</H2>
-          <ColThree>
-            <article>
-              <h3>React.js</h3>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse ab quae molestias laboriosam beatae vero ipsa quam vitae quaerat aspernatur porro reprehenderit excepturi est libero natus cupiditate, itaque quidem! Repellat.</p>
-            </article>
-            <article>
-              <h3>Node/Express</h3>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam officiis ab eveniet harum nostrum placeat perspiciatis exercitationem tempore dolores ducimus quod, consectetur est a voluptates omnis. Quasi ex possimus molestiae!</p>
-            </article>
-            <article>
-              <h3>Framer Motion</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nam voluptate cum architecto deserunt asperiores dicta beatae accusantium veritatis repellat! Reprehenderit non, sed odio iste deleniti ipsa blanditiis enim recusandae!</p>
-            </article>
-          </ColThree>
-          <a href="#">Examples</a>
-        </section>
-        <section>
-          <H2>A little about me...</H2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, dignissimos perferendis enim velit modi non quae possimus cupiditate assumenda placeat vero sapiente voluptas id similique, veritatis impedit culpa tempore deserunt!</p>
-          <div>
-            <p></p>
-            <p></p>
-          </div>
-        </section>
-        <footer>
-          <a href="mailto:johnbarhorst.dev@gmail.com">John Barhorst</a>
-        </footer>
-      </Wrapper>
-    </AnimatePresence>
+      <section>
+        <H2>What I Use</H2>
+        <ColThree>
+          <article>
+            <h3>React.js</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse ab quae molestias laboriosam beatae vero ipsa quam vitae quaerat aspernatur porro reprehenderit excepturi est libero natus cupiditate, itaque quidem! Repellat.</p>
+          </article>
+          <article>
+            <h3>Node/Express</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam officiis ab eveniet harum nostrum placeat perspiciatis exercitationem tempore dolores ducimus quod, consectetur est a voluptates omnis. Quasi ex possimus molestiae!</p>
+          </article>
+          <article>
+            <h3>Framer Motion</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nam voluptate cum architecto deserunt asperiores dicta beatae accusantium veritatis repellat! Reprehenderit non, sed odio iste deleniti ipsa blanditiis enim recusandae!</p>
+          </article>
+        </ColThree>
+        <Link to='/portfolio'>Examples</Link>
+      </section>
+      <section>
+        <H2>A little about me...</H2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, dignissimos perferendis enim velit modi non quae possimus cupiditate assumenda placeat vero sapiente voluptas id similique, veritatis impedit culpa tempore deserunt!</p>
+        <div>
+          <p></p>
+          <p></p>
+        </div>
+      </section>
+      <footer>
+        <a href="mailto:johnbarhorst.dev@gmail.com">John Barhorst</a>
+      </footer>
+    </Wrapper>
   )
 }
 
 export default Main;
-
-const Wrapper = styled(motion.section)`
-  max-width: 90%;
-  margin: 0 auto;
-`;
 
 const ProfilePic = styled(motion.img)`
 `;
