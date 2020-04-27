61a9ee21fff625249c91d96b4b4ec22995f4efeb
John Barhorst <crashdsinc@comcast.net>
2020-04-26 19:33:05 -0500

    start fleshing this out more

--- client/src/Components/Destiny/DestinyDemo.js
+++ client/src/Components/Destiny/DestinyDemo.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import { Switch, Route } from 'react-router-dom';
 import SearchDisplay from './SearchDisplay';
 import SearchForm from './SearchForm';
+import { H3, Wrapper } from '../../Elements';
 
 const DestinyDemo = () => {
 
   return (
-    <motion.div
+    <Wrapper
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
+      <H3>Search for a Destiny 2 player screen name.</H3>
       <SearchForm />
       <Switch>
         <Route exact path='/destiny' component={SearchDisplay} />
       </Switch>
-    </motion.div>
+    </Wrapper>
   )
 }
 
