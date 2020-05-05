9b9cb3555206ea094e691e7bba7f738274120801
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-05-04 23:02:35 -0500

    stat ordering and start of hover card

--- client/src/Components/Destiny/CharacterStatCard.js
+++ client/src/Components/Destiny/CharacterStatCard.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { useHover } from '../../Hooks';
+
+const ToolTip = ({ children }) => {
+  return (
+    <motion.div>
+      {children}
+    </motion.div>
+  )
+}
 
 const CharacterStatCard = ({ stats }) => {
+  const [isHovered, bind, [enterX, enterY], [exitX, exitY]] = useHover();
+  console.log('test', isHovered);
+  console.log('x y', enterX, enterY, exitX, exitY);
   return (
     <div>
       {stats.sort((a, b) => {
-        const order = ['power', 'mobility', 'resilience', 'intellect', 'strength', 'discipline', 'recovery'];
+        const order = ['power', 'mobility', 'resilience', 'recovery', 'discipline', 'intellect', 'strength'];
         return order.indexOf(a.name.toLowerCase()) - order.indexOf(b.name.toLowerCase())
       }).map(({ name, icon, value }) => (
-        <div key={name}>
+        <div key={name} {...bind}  >
           <p>{name}</p>
           <Img src={`https://www.bungie.net${icon}`} alt={name} /><span>{value}</span>
         </div>
