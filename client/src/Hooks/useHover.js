a3b4c736b789963c00f05c07753f64a283a1a819
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-05-04 22:20:22 -0500

    expanded to show x and y

--- client/src/Hooks/useHover.js
+++ client/src/Hooks/useHover.js
@@ -2,11 +2,21 @@ import { useMemo, useState } from 'react';
 
 export const useHover = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [[enterX, enterY], setEnterXY] = useState([null, null]);
+  const [[exitX, exitY], setExitXY] = useState([null, null]);
+
+
   const bind = useMemo(() => {
     return {
-      onMouseOver: () => setIsHovered(true),
-      onMouseLeave: () => setIsHovered(false)
+      onMouseOver: (e) => {
+        setEnterXY([e.clientX, e.clientY]);
+        setIsHovered(true);
+      },
+      onMouseLeave: (e) => {
+        setExitXY([e.clientX, e.clientY]);
+        setIsHovered(false);
+      }
     }
   }, []);
-  return [isHovered, bind]
+  return [isHovered, bind, [enterX, enterY], [exitX, exitY]]
 }
