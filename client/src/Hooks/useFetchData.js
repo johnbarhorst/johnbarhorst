7c39591799101f212cb67695dc0de6113b99bf45
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-05-06 22:20:33 -0500

    fetch with effect version. Still getting ex deps

--- client/src/Hooks/useFetchData.js
+++ client/src/Hooks/useFetchData.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -50,3 +50,31 @@ export const useFetchData = (initialData) => {
 
   return [state, goFetch];
 }
+
+export const useFetchOnLoad = (url, options, initialData) => {
+  const [state, dispatch] = useReducer(reducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    const goFetch = async (url, options) => {
+      dispatch({ type: 'FETCH' });
+      try {
+        const req = await fetch(url, options);
+        const json = await req.json();
+        if (json.status === 200) {
+          dispatch({ type: 'SUCCESS', payload: json });
+        }
+        if (json.status === 404) {
+          dispatch({ type: 'ERROR', payload: json });
+        }
+      } catch (error) {
+        dispatch({ type: 'ERROR' });
+      }
+    }
+    goFetch(url, options);
+  }, [])
+  return [state]
+};
