4710856622bf3b1899abc14a175a08ecd5103e27
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-05-20 16:43:08 -0500

    masterwork border

--- client/src/Components/Destiny/Item.js
+++ client/src/Components/Destiny/Item.js
@@ -5,10 +5,10 @@ import styled from 'styled-components';
 const hasEnergy = obj => Object.keys(obj).length === 0 ? false : true;
 
 const Item = ({ item }) => {
-  const { name, hasIcon, icon, itemTypeDisplayName, damageType, energy } = item;
+  const { name, hasIcon, icon, itemTypeDisplayName, damageType, energy, masterwork } = item;
   return (
     <ItemWrapper>
-      {hasIcon && <Img src={`https://www.bungie.net${icon}`} />}
+      {hasIcon && <Img src={`https://www.bungie.net${icon}`} isMasterworked={masterwork} />}
       <div>
         <p><strong>{name}</strong></p>
         <p>{itemTypeDisplayName}</p>
@@ -24,12 +24,14 @@ export default Item;
 const Img = styled.img`
   height: 96px;
   width: 96px;
+  border: 2px solid ${props => props.isMasterworked ? 'gold' : 'white'};
 `;
 
 const ItemWrapper = styled.div`
   display: grid;
   gap: .5em;
-  grid-template-columns: auto 1fr;
+  grid-template-columns: 100px 1fr;
+  margin-bottom: .5em;
   p {
     margin: 0;
     text-transform: capitalize;
