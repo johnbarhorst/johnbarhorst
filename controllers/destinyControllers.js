b1922fed9fea9c74ec5b35b6387135530ee8e26c
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-05-31 20:14:27 -0500

    reorganized a lot of stuff, more comments

--- controllers/destinyControllers.js
+++ controllers/destinyControllers.js
@@ -91,52 +91,22 @@ const getDetailsAll = async (object, table, callback) => {
 // A giant mess of a function that returns a whole bunch of character data, processed with live data
 // from the API, as well as static data from the sqlite database.
 const processCharacters = async (data) => {
-  // References as listed on in the API docs. All sources say this info is highly unlikely to change, however if
-  // you start getting the wrong info, you can double check at https://bungie-net.github.io/multi/
-  const classTypeRef = ["Titan", "Hunter", "Warlock"];
-  const genderTypeRef = ["Male", "Female"];
-  const raceTypeRef = ["Human", "Awoken", "Exo"];
+
+  // Take the character ID numbers for each guardian and put them in an array,
+  // We use them down below with a map function to then gather all the details on all the items.
+  const characterArray = Array.from(Object.values(data.characters.data));
+
   const damageTypeEnum = [null, 'kinetic', 'arc', 'solar', 'void', 'raid'];
-  const energyTypeEnum = [null, 'arc', 'solar', 'void']; // So far looks like armor has this info in the energy db entry.
+  //const energyTypeEnum = [null, 'arc', 'solar', 'void']; // So far looks like armor already has this info in the energy db entry.
 
   // shorthand for parts of the data object from bungie
+  // instanced data for the whole account comes in one bunch. Which character currently has it is determined on a
+  // character level. There is a mass of data in this scope, then sort it into each character below.
   const instances = data.itemComponents.instances.data;
   const sockets = data.itemComponents.sockets.data;
   const stats = data.itemComponents.stats.data;
 
-  // Character title details
-  // TODO: Test against a character with no title, then format this properly with only the info we need.
-  const getTitleDetails = async character => {
-    return character.titleRecordHash ? await getFromDB(character.titleRecordHash, 'DestinyRecordDefinition') : null
-  }
-
-  // Armor Energy definitions from DB along with the instanced data from API
-  const getEnergyDetails = async item => {
-    const details = await getFromDB(item.energyTypeHash, 'DestinyEnergyTypeDefinition');
-    return {
-      ...details.displayProperties,
-      transparentIconPath: details.transparentIconPath,
-      capacity: item.energyCapacity,
-      used: item.energyUsed,
-      unused: item.energyUnused
-    }
-  }
-
-  // Socket definitions from the DB for instanced items from the API
-  const getSocketDetails = async sockets => {
-    const values = Array.from(Object.values(sockets));
-    const details = await Promise.all(values.map(async socket => {
-      const data = await getFromDB(socket.plugHash, 'DestinyInventoryItemDefinition');
-      const displayProperties = data.displayProperties ? data.displayProperties : {};
-      return {
-        ...socket,
-        ...displayProperties
-      }
-    }))
-    return details;
-  }
-
-  // The big congregation of data for an individual character.
+  // Get details on all equipment items from the DB and match them up with instanced data from Bungie's api:
   const getGuardianEquipmentDetails = async equipment => {
     const itemsWithDetails = await Promise.all(equipment.map(async item => {
       try {
@@ -154,12 +124,53 @@ const processCharacters = async (data) => {
         // so that later on a .map() will return nothing instead of throwing an error.
         const instancedSockets = (sockets[item.itemInstanceId] || { sockets: [] }).sockets;
 
+        // Get definitions for instanced stats on the item from the DB, and return the details we want from each.
+        const instanceStats = await getDetailsAll(instancedStats, 'DestinyStatDefinition', (item, details) => {
+          return {
+            //uncomment the lines below to send along original item data and/or database details
+            // originalData: {...item},
+            // dbDetails: {...details},
+            ...details.displayProperties,
+            value: item.value,
+            displayMaximum: item.displayMaximum,
+          };
+        });
+
+        //Gets details for an items primary stats, if there is one. This is usually Attack/Defence and just another name
+        // for their light level. Exceptions for sparrows and artifacts.
         const getPrimaryStatDetails = async stat => {
           const details = await getFromDB(stat.statHash, 'DestinyStatDefinition');
           return {
             ...details.displayProperties,
             value: stat.value,
           }
+        }
+
+        // Armor Energy definitions from DB along with the instanced data from API
+        const getEnergyDetails = async item => {
+          const details = await getFromDB(item.energyTypeHash, 'DestinyEnergyTypeDefinition');
+          return {
+            ...details.displayProperties,
+            transparentIconPath: details.transparentIconPath,
+            capacity: item.energyCapacity,
+            used: item.energyUsed,
+            unused: item.energyUnused
+          }
+        }
+
+        // Socket definitions from the DB for instanced items from the API
+        // We don't use getDetailsAll here because the values are the hashes for sockets, not the object keys.
+        const getSocketDetails = async sockets => {
+          const values = Array.from(Object.values(sockets));
+          const details = await Promise.all(values.map(async socket => {
+            const data = await getFromDB(socket.plugHash, 'DestinyInventoryItemDefinition');
+            const displayProperties = data.displayProperties ? data.displayProperties : {};
+            return {
+              ...socket,
+              ...displayProperties
+            }
+          }))
+          return details;
         }
 
         // Take all the processed data to return only what we want to display.
@@ -169,26 +180,15 @@ const processCharacters = async (data) => {
           screenshot: details.screenshot,
           itemTypeDisplayName: details.itemTypeDisplayName,
           displaySource: details.displaySource,
-          // TODO This got borked. Don't even think I need it, yet I want to fix it.
-          // staticStats: await getDetailsAll(details.stats.stats, 'DestinyStatDefinition', (item, details) => {
-          //   return {
-          //     ...details.displayProperties,
-          //     value: item.value,
-          //     displayMaximum: item.displayMaximum,
-          //   };
-          // }),
-          instanceStats: await getDetailsAll(instancedStats, 'DestinyStatDefinition', (item, details) => {
-            return {
-              ...details.displayProperties,
-              value: item.value,
-              displayMaximum: item.displayMaximum,
-            };
-          }),
-          damageType: damageTypeEnum[instanceDetails.damageType],
-          energy: instanceDetails.energy ? await getEnergyDetails(instanceDetails.energy) : {},
-          sockets: instancedSockets ? await getSocketDetails(instancedSockets) : {},
+          instanceStats: instanceStats,
           itemType: details.itemType,
+          damageType: damageTypeEnum[instanceDetails.damageType],
+          // Only armor has energy right now.
+          energy: instanceDetails.energy ? await getEnergyDetails(instanceDetails.energy) : null,
+          // Not all items have sockets
+          sockets: instancedSockets ? await getSocketDetails(instancedSockets) : null,
           itemCategoryHashes: await Promise.all(details.itemCategoryHashes.map(async hash => await getFromDB(hash, 'DestinyItemCategoryDefinition'))),
+          // State: none is 0, locked is 1, masterworked is 4. So locked and master worked is 5
           masterwork: item.state === 4 || item.state === 5 ? true : false,
           primaryStat: instanceDetails.primaryStat ? await getPrimaryStatDetails(instanceDetails.primaryStat) : null,
           // Uncomment these to see all the original data, in case you want to dig and find other things to display
@@ -198,7 +198,10 @@ const processCharacters = async (data) => {
           // originalSockets: { ...instancedSockets },
           // originalDetails: { ...details },
         };
+
       } catch {
+        // TODO: So far, the only issues I've had invole having an outdated manifest DB from bungie.
+        // Likely this isn't the only error possible, but for now this is a decent catch.
         return {
           ...item,
           errorMessage: 'Probably need to update the database'
@@ -208,21 +211,11 @@ const processCharacters = async (data) => {
     return itemsWithDetails;
   }
 
-  const characterArray = Array.from(Object.values(data.characters.data));
+
+
+  // Take the array of character IDs from way up top, and 
   const charactersWithDetails = await Promise.all(characterArray.map(async character => {
-
-    // Uncomment these AND the pvp/pve stats down at the bottom of this function to send
-    // weapon kill stats along with the character data call.
-    // Making 3 more API calls in with the character data does make the whole call take considerably longer,
-    // so I'm leaning towards making this a separate route to be hit when loading an individual character page.
-
-    // const historicalStats = await fetch(
-    //   `https://www.bungie.net/Platform/Destiny2/${character.membershipType}/Account/${character.membershipId}/Character/${character.characterId}/Stats/?groups=Weapons`,
-    //   { headers }).then(res => res.json());
-    // const historyStatus = checkStatus(historicalStats);
-    // const pvpStats = historyStatus ? historicalStats.Response.allPvP.allTime : {};
-    // const pveStats = historyStatus ? historicalStats.Response.allPvE.allTime : {};
-
+    // Destructure all the stuff we're gonna use
     const {
       membershipId,
       membershipType,
@@ -238,6 +231,30 @@ const processCharacters = async (data) => {
       emblemPath,
       stats
     } = character;
+
+    // References as listed on in the API docs. All sources say this info is highly unlikely to change, however if
+    // you start getting the wrong info, you can double check at https://bungie-net.github.io/multi/
+    const classTypeRef = ["Titan", "Hunter", "Warlock"];
+    const genderTypeRef = ["Male", "Female"];
+    const raceTypeRef = ["Human", "Awoken", "Exo"];
+
+    const equipment = await getGuardianEquipmentDetails(data.characterEquipment.data[character.characterId].items);
+
+    // Look up character title details
+    const getTitleDetails = async character => {
+      const details = character.titleRecordHash ? await getFromDB(character.titleRecordHash, 'DestinyRecordDefinition') : null;
+      if (details) {
+        return {
+          ...details.displayProperties,
+          hasTitle: details.titleInfo.hasTitle,
+          titlesByGender: { ...details.titleInfo.titlesByGender }
+        }
+      } else {
+        return {
+          hasTitle: false
+        }
+      }
+    }
 
     return {
       membershipId,
@@ -259,14 +276,7 @@ const processCharacters = async (data) => {
           value: stat
         }
       }),
-      equipment: await getGuardianEquipmentDetails(data.characterEquipment.data[character.characterId].items),
-
-      // Uncomment these next two lines, AND the API call for stats further up to send stats along with the character data call 
-      // pvpStats,
-      // pveStats,
-
-      // Uncomment this to get all account stats along with the API call up above to send kill stats along for the ride
-      // historicalStats
+      equipment,
     }
   }))
   return charactersWithDetails;
@@ -275,13 +285,12 @@ const processCharacters = async (data) => {
 exports.getCharacterInfo = async (req, res) => {
   const accountData = await fetch(
     `https://www.bungie.net/Platform/Destiny2/${req.params.membershipType}/Profile/${req.params.membershipId}/?components=100,104,200,205,300,304,305,1100`,
-    { headers }).then(res => res.json());
+    { headers }).then(res => res.status === 200 ? res.json() : new Error(res));
   const responseStatus = checkStatus(accountData);
   // console.log(accountData);
   if (responseStatus) {
     const characters = await processCharacters(accountData.Response);
     const profileInfo = {
-      status: 200,
       test: 'test',
       characters: characters,
 
@@ -293,12 +302,13 @@ exports.getCharacterInfo = async (req, res) => {
       // plugs: await getDetailsAll(accountData.Response.profilePlugSets.data.plugs, 'DestinyPlugSetDefinition'),
 
     };
+    res.status = 200;
     res.json(profileInfo);
   } else {
     const ErrorResponse = {
-      status: 404,
       ...accountData
     }
+    res.status = 404;
     res.json(ErrorResponse);
   }
 }
@@ -325,6 +335,7 @@ exports.getCurrentManifest = async (req, res) => {
     ));
 
   // Just sending something here to finish the browser call to the server. This shouldn't be necessary once I make it a cron job.
+  res.status = 200;
   res.send('Check data');
 }
 
@@ -334,14 +345,14 @@ exports.searchAccounts = async (req, res) => {
     { headers }).then(res => res.json());
   const responseStatus = checkStatus(searchQuery);
   if (responseStatus) {
+    res.status = 200;
     const accountList = {
-      status: 200,
       accounts: searchQuery.Response
     };
     res.send(JSON.stringify(accountList));
   } else {
+    res.status = 401;
     const errorStatus = {
-      status: 401,
       errorMessage: searchQuery.Message
     }
     res.json(errorStatus);
