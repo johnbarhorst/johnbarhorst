964f3390217ec7da088dbf801879727a4e4b7959
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-06-21 14:59:40 -0500

    Stat bars!

--- client/src/Components/Destiny/InstanceStatsCard.js
+++ client/src/Components/Destiny/InstanceStatsCard.js
@@ -1,16 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const InstanceStatsCard = ({ instanceStats }) => {
   // Need to order stats.
-  // Most stats have a bar, but some do not, need to differentiate.
   // Some weapons (ie swords) have unique stats, armor has different stats than weapons.
 
+  const sort = (a, b) => {
+    if (a.displayAsNumeric === b.displayAsNumeric) {
+      return a.index - b.index;
+    }
+    return a.displayAsNumeric - b.displayAsNumeric
+  }
   return (
     <div className={'full-span'}>
-      {instanceStats.sort((a, b) => a.index - b.index).map((stat, i) => <p key={stat.statHash}>{stat.name}: {stat.value}</p>)}
+      {instanceStats.sort((a, b) => sort(a, b)).map(stat => <StatBar key={stat.statHash} {...stat} />)}
     </div>
   )
 }
 
 export default InstanceStatsCard;
+
+const StatBar = ({ name, value, displayAsNumeric, maximumValue }) => {
+  return (
+    <WeaponStatDisplay>
+      {displayAsNumeric ?
+        (<><p className={'text-right'}>{name}</p><p>{value}</p></>) :
+        (<><p className={'text-right'}>{name}</p> <AnimatedStatBar value={value}><div></div></AnimatedStatBar></>)
+      }
+    </WeaponStatDisplay>
+  )
+}
+
+const WeaponStatDisplay = styled(motion.div)`
+  display: grid;
+  grid-template-columns: 1fr 60%;
+  gap: .75rem;
+  .text-right {
+    text-align: right;
+  }
+`;
+
+const AnimatedStatBar = styled(motion.div)`
+  background: ${props => props.theme.background};
+  height: 70%;
+  width: 90%;
+  align-self: center;
+div {
+  width: ${props => props.value}%;
+  height: 100%;
+  background: ${props => props.theme.color};
+}
+`;
