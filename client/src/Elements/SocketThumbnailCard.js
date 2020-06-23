39e02a0fe291282c78e065ffef2d4a385216a360
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-06-22 22:36:06 -0500

    socket stuff. Might not need this?

---
+++ client/src/Elements/SocketThumbnailCard.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const SocketThumbnailCard = styled(motion.div)`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
