9ffc2b47f0ad419c5b2d6c83c43de82cab741486
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-06-22 22:35:50 -0500

    prep for full expand

--- client/src/Elements/ItemCard.js
+++ client/src/Elements/ItemCard.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 export const ItemCard = styled(motion.div)`
+  ${props => props.background && `background-image: url('http://www.bungie.net${props.background}');
+  background-size: cover;
+  background-repeat: no-repeat;`}
   display: grid;
   gap: 0 .25rem;
   grid-template-columns: 1fr 2fr 1fr;
