ac36efeb11a916ace823e1429e40d7a1d81c8c32
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-06-23 19:43:40 -0500

    But really, why did you render?

---
+++ client/src/wdyr.js
@@ -0,0 +1,9 @@
+// WHY DID YOU RENDER!?
+import React from 'react';
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+    trackHooks: true
+  });
+}
