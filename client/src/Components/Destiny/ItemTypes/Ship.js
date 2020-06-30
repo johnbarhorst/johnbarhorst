7915a8a675f356c600fb9fffbac20950d2094189
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-06-29 21:52:46 -0500

    ship screenshots so far are smaller

--- client/src/Components/Destiny/ItemTypes/Ship.js
+++ client/src/Components/Destiny/ItemTypes/Ship.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import Sockets from '../Sockets';
 import { useToggle } from '../../../Hooks';
 import { ItemIcon, ItemWrapper, ItemCard } from '../../../Elements';
@@ -8,18 +10,26 @@ export const Ship = ({ icon, masterwork, sockets, name, itemTypeDisplayName, scr
   const [isExpanded, toggleExpanded] = useToggle(false, true);
   return (
     <ItemWrapper>
-      <ItemCard
+      <ShipCard
         onClick={() => toggleExpanded(isExpanded => !isExpanded)}
         background={screenshot}
         className={isExpanded && 'isExpanded'}
       >
-        <ItemIcon src={`https://www.bungie.net${icon}`} isMasterworked={masterwork} />
+        <ItemIcon src={`https://www.bungie.net${icon}`} isMasterworked={masterwork} positionTransition />
         <div>
-          <p><strong>{name}</strong></p>
-          <p>{itemTypeDisplayName}</p>
+          <motion.p positionTransition key={name}><strong>{name}</strong></motion.p>
+          <motion.p positionTransition key={itemTypeDisplayName}>{itemTypeDisplayName}</motion.p>
         </div>
         <Sockets sockets={sockets} isExpanded={isExpanded} />
-      </ItemCard>
+      </ShipCard>
     </ItemWrapper>
   )
 }
+
+
+// Ship screenshots appear to be less wide than other items.
+const ShipCard = styled(ItemCard)`
+  &.isExpanded {
+    background-size: cover;
+  }
+`;
